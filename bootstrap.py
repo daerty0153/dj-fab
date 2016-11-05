a8bd7219045caeaf0180abb1347b960726a5413b
saurabh <saurabh@kuliza.com>
2016-11-06 01:22:22 +0530

    update settings and comment

--- bootstrap.py
+++ bootstrap.py
@@ -21,8 +21,9 @@ env_name = configParser.get('project-settings', 'env_name')
 code_repo = configParser.get('repo-settings', 'repo_name')
 path = configParser.get('project-path', 'env_path')
 requirements_file = configParser.get('project-settings', 'requirements_file')
+my_host = configParser.get('project-settings', 'my_host')
 
-@hosts('127.0.0.1')
+@hosts(my_host)
 def main():
     if "VIRTUAL_ENV" not in os.environ:
         # Create virtual environment
@@ -31,7 +32,9 @@ def main():
         # Move inside virtualenv and setup code
         with cd('%s%s/' % (path, env_name)):
             with prefix('source %s%s/bin/activate' % (path, env_name)):
+                # Clone code base to local (Assumes you're using git)
                 run('git clone %s' % code_repo)
+                # Installing requirements
                 run('pip install -r %s' % requirements_file)
                 #sys.stderr.write("$VIRTUAL_ENV not found.\n\n")
                 #parser.print_usage()
